f5b4785a692500d8a47e8acd77e4769affa20f26
prakhar17072004 <pmankur2004@gmail.com>
2024-08-02 01:23:35 +0530

    navbar add

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import logo  from "../assets/logo.png"
 function Navbar() {
   return (
     <div className='flex  justify-between bg-gray-800 text-white p-8'>
    <div className='flex'> 
-     <img src="/" alt='log pic'/>
+     <img src={logo} alt='logo pic' height="30px" width="50px"/>
    </div>
    <div  className='flex justify-evenly gap-8'>
    <Link  to="/">
@@ -14,7 +14,7 @@ function Navbar() {
   <Link  to="admin">
   <p>ADMIN</p>
   </Link>
-  <Link  to=" cart">
+  <Link  to="cart">
   <p>CART</p>
   </Link>
    </div>
